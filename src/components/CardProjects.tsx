e9009d4b4a8afd01e4c1f77d341f6f410dc8678a
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-01 00:18:22 -0300

    realizamos la seccion del Projects con su componente correspondiente, tambien cargamos el tipado de los datos

---
+++ src/components/CardProjects.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { APIResult } from "../types";
+
+interface CardProjectsProps {
+  projects: APIResult[];
+}
+
+const CardProjects: React.FC<CardProjectsProps> = ({ projects }) => {
+  const sortedProjects = [...projects].sort(
+    (a, b) => parseInt(b.id) - parseInt(a.id)
+  );
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      {sortedProjects.map((project) => (
+        <div
+          key={project.id}
+          className="bg-[#1B1E22] shadow-md rounded p-4 mb-4"
+        >
+          <div>
+            <img src={project.image} alt="" />
+          </div>
+          <h2 className="text-xl font-bold">{project.nombre}</h2>
+          <p>{project.description}</p>
+          <div className="mt-2">
+            {project.tecnhologies.map((tech, index) => (
+              <span
+                key={index}
+                className="inline-block bg-gray-200 text-gray-700 rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2"
+              >
+                {tech}
+              </span>
+            ))}
+          </div>
+          <div className="flex justify-around py-2">
+            <a href={project.github}>
+              <button className="bg-slate-950 hover:bg-slate-900 py-1 px-5 rounded-md">
+                Codigo
+              </button>
+            </a>
+            <a href={project.test}>
+              <button className="bg-slate-950 hover:bg-slate-900 py-1 px-5 rounded-md">
+                Prueba
+              </button>
+            </a>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CardProjects;
---
+++ src/components/Projects.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CardProjects from "./CardProjects";
+import Dates from "../mock/Projects.json";
+import { APIResult } from "../types";
+
+// Necesario para importar JSON en TypeScript
+const projects: APIResult[] = Dates;
+
+export const Projects: React.FC = () => {
+  return (
+    <section id="projects" className="text-white flex justify-center my-14">
+      <div className="w-[1200px]">
+        <div className="mx-5">
+          <div className="text-center">
+            <h2 className="text-3xl">
+              <strong>Proyectos</strong>
+            </h2>
+          </div>
+          <div className="py-10">
+            <CardProjects projects={projects} />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
