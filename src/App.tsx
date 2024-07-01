c1590af6c558c1ba744db800f578d5e26fbafd16
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-30 23:21:42 -0300

    se agrega el Hero donde detallo una presentacion mia

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/Header";
+import { Hero } from "./components/Hero";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 };
