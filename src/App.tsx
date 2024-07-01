025ddff14b47dace7fefea359dcb917d4184b040
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-06-30 22:02:37 -0300

    se realiza un header con modelo responsive y botones a las futuras secciones a agregar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Header } from "./components/Header";
 
 export const App = () => {
   return (
     <>
-      <h1>Pagina Principal</h1>
+      <Header />
     </>
   );
-}
+};
