a2b7412349d3e3199c962a65df118fac05b35945
Hector Clessi <121068920+hectorc2907@users.noreply.github.com>
2024-07-01 02:54:12 -0300

    se realiza la configuracion del footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Contact } from "./components/Contact";
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
 import { Projects } from "./components/Projects";
@@ -13,6 +14,7 @@ export const App = () => {
       <Projects />
       <Technologies />
       <Contact />
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+  return (
+    <footer className="flex justify-center py-10">
+      <div className="grid grid-cols-1 gap-5">
+        <div className="flex justify-center">
+          <img src="../../public/img/logo.png" className="w-[250px]" alt="" />
+        </div>
+        <div>
+          <p className="text-white">HAC - Software © Derechos Reservados</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
